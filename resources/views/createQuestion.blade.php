ed8cd27c11bddfc0cdcc2253b7c282bc3b78fc81
Mike <mikedejong4996@gmail.com>
2021-01-13 15:59:57 +0100

    html gefixt, docent vragen pagina, seeder aangepast
    de vragen zijn nu te zien op de docent beheer vragen pagina

--- resources/views/createQuestion.blade.php
+++ resources/views/createQuestion.blade.php
@@ -10,18 +10,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <body>
-            <form method="post" action="{{url('/submitQuestion') }}">
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">question</label>
-                    <br>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="6" placeholder="een vraag?" required minlength="20" type='text'name='question'></textarea>
-                </div>
-                {{ csrf_field() }}
-                <button type="submit" class="p-2 bg-blue-500 w-25 rounded shadow text-white">submit</button>
-            </form>
-            </body>
+                <body>
+                    <form method="post" action="{{url('/submitQuestion') }}">
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">question</label>
+                            <br>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="6" placeholder="een vraag?" required minlength="20" type='text'name='question'></textarea>
+                        </div>
+                        {{ csrf_field() }}
+                        <button type="submit" class="p-2 bg-blue-500 w-25 rounded shadow text-white">submit</button>
+                    </form>
+                </body>
             </div>
-            </div>
-            </div>
+        </div>
+    </div>
 </x-app-layout>
