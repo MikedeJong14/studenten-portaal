0b4b59f8edc9d6dcfa621b9fd323af0676714c82
99051257 <99051257@mydavinci.nl>
2021-02-12 15:22:52 +0100

    Started Dashboard styling

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,9 +14,13 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    public function index() {
+        $user = (object) [
+            'name' => Auth::user()->name,
+            'appointment' => Auth::user()->appointments->where('date', '>=', date("Y-m-d"))->first(),
+        ];
+        
+        return view('dashboard', ['user' =>$user]);
     }
 
     /**
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
-                <p>Welkom bij de studenten portaal applicatie</p>
-                <br>
-                <p>Dit is de Dashboard pagina</p>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 my-3">
+                <div class="bg-white p-5 my-3">
+                    <p class="text-5xl font-semibold">Welkom {{ $user->name }}</p>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 my-3">
+                <div class="p-5 my-3">
+                    <p class="text-xl p-5">Eerstvolgende afspraak:</p>
+                </div>
             </div>
         </div>
     </div>
