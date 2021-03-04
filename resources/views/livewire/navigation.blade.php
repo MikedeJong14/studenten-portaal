7e9c25300b0bdc2c173664ac08302495fdf2f72e
Rutger Vos <99033234@mydavinci.nl>
2021-03-04 11:23:32 +0100

    taal fix

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -2,7 +2,7 @@
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
         @auth
-            <a href="{{ route('dashboard') }}" class="text-sm text-white underline">to Dashboard</a>
+            <a href="{{ route('dashboard') }}" class="text-sm text-white underline">naar Dashboard</a>
         @else
             <a href="{{ route('login') }}" class="text-sm text-white underline">Login</a>
         @endauth
